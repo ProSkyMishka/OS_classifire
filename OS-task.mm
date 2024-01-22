f46eef69f076471f4529819521929cef1a4bf4ab
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-01-22 14:26:17 +0300

    Add files via upload

---
+++ OS-task.mm
@@ -0,0 +1,236 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ОС" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1705919421709" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_sky_element_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.22048 pt;0 pt;" ENDINCLINATION="116.22048 pt;28.34646 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="16" BOLD="true"/>
+<node TEXT="По назначению" POSITION="top_or_left" ID="ID_1851519598" CREATED="1704713226367" MODIFIED="1704713234817">
+<node TEXT="ОС мейнфреймов" ID="ID_1801262056" CREATED="1704713248358" MODIFIED="1705919837559" VGAP_QUANTITY="3 pt">
+<node TEXT="OS/360&#xa;VME" ID="ID_1857831971" CREATED="1704802172752" MODIFIED="1705920012689" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Серверные ОС" ID="ID_624224166" CREATED="1704713322614" MODIFIED="1704713329416">
+<node TEXT="FreeBSD&#xa;Windows Server 201" ID="ID_1946930052" CREATED="1704802172752" MODIFIED="1705920007241" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Многопроцессорные ОС" ID="ID_1115840905" CREATED="1704713334102" MODIFIED="1704713341505">
+<node TEXT="macOs&#xa;Linux" ID="ID_1029633872" CREATED="1704802172752" MODIFIED="1705920399294" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="ОС персональных компьютеров" ID="ID_70707584" CREATED="1704713350733" MODIFIED="1704713361880">
+<node TEXT="macOS&#xa;Windows 7" ID="ID_1496129987" CREATED="1704802172752" MODIFIED="1705920425725" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="ОС мобильных систем" ID="ID_151815302" CREATED="1704713378614" MODIFIED="1704713390640">
+<node TEXT="Карманные персональные компьютеры" ID="ID_352566054" CREATED="1704713393454" MODIFIED="1704713409160">
+<node TEXT="Android&#xa;IOS" ID="ID_1287198055" CREATED="1704802172752" MODIFIED="1705920448633" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Планшеты" ID="ID_1409791043" CREATED="1704713409694" MODIFIED="1704713414193">
+<node TEXT="Android&#xa;iPadOS" ID="ID_1659566668" CREATED="1704802172752" MODIFIED="1705920515636" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Коммуникаторы, смартфоны, телефоны" ID="ID_999325026" CREATED="1704713415294" MODIFIED="1704713447256">
+<node TEXT="EMUI&#xa;IOS" ID="ID_1794534155" CREATED="1704802172752" MODIFIED="1705920550998" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+<node TEXT="Встроенные ОС" ID="ID_1693427078" CREATED="1704713497966" MODIFIED="1704713504133">
+<node TEXT="Embedded Linux&#xa;QNX" ID="ID_1607847870" CREATED="1704802172752" MODIFIED="1705920630789" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="ОС сенсорных узлов" ID="ID_485281571" CREATED="1704713505286" MODIFIED="1704713520664">
+<node TEXT="TinyOS&#xa;RIOT" ID="ID_1349763674" CREATED="1704802172752" MODIFIED="1705920646180" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="ОС реального времени" ID="ID_106394579" CREATED="1704713562877" MODIFIED="1704713579624">
+<node TEXT="eCos&#xa;FreeRTOS" ID="ID_467282217" CREATED="1704802172752" MODIFIED="1705920660677" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+<node TEXT="Унификация" POSITION="bottom_or_right" ID="ID_1039594994" CREATED="1704713688894" MODIFIED="1704713697272">
+<node TEXT="По архитектуре компьютера" ID="ID_1068681908" CREATED="1704713704309" MODIFIED="1704713747544">
+<node TEXT="x86&#xa;ARM" ID="ID_1325699429" CREATED="1704802172752" MODIFIED="1705922476223" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="По производительности" ID="ID_1846008257" CREATED="1704729379616" MODIFIED="1704729390078">
+<node TEXT="Linux&#xa;macOS" ID="ID_752052294" CREATED="1704802172752" MODIFIED="1705921840763" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="По целевому назначению" ID="ID_1796688645" CREATED="1704713785269" MODIFIED="1704713794961">
+<node TEXT="серверные ОС&#xa;мобильные ОС&#xa;встроенные ОС" ID="ID_1717486302" CREATED="1704802172752" MODIFIED="1705921821438" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+<node TEXT="По группе пользователей" POSITION="top_or_left" ID="ID_1665981910" CREATED="1704713851877" MODIFIED="1704713895049">
+<node TEXT="Прикладные пользователи" ID="ID_1916463909" CREATED="1704713882534" MODIFIED="1704713910823">
+<node TEXT="Windows&#xa;macOS" ID="ID_1897064533" CREATED="1704802172752" MODIFIED="1705920696179" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Программисты" ID="ID_280632179" CREATED="1704713915214" MODIFIED="1704713922896">
+<node TEXT="Linux&#xa;Mac OS" ID="ID_1534888856" CREATED="1704802172752" MODIFIED="1705920740695" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Системные администраторы" ID="ID_1146217917" CREATED="1704713987781" MODIFIED="1704713995544">
+<node TEXT="Linux&#xa;FreeBSD" ID="ID_1362277512" CREATED="1704802172752" MODIFIED="1705920800238" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+<node TEXT="Архитектурные решения" POSITION="bottom_or_right" ID="ID_370998678" CREATED="1704714016286" MODIFIED="1704714029599">
+<node TEXT="Монолитная архитектура" ID="ID_1455172383" CREATED="1704714034254" MODIFIED="1704714073536">
+<node TEXT="UNIX&#xa;Novell NetWare" ID="ID_1759152997" CREATED="1704802172752" MODIFIED="1705921805206" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Многоуровневая архитектура" ID="ID_78376677" CREATED="1704714052622" MODIFIED="1704714060400">
+<node TEXT="THE&#xa;MULTICS" ID="ID_1030168498" CREATED="1704802172752" MODIFIED="1705921548654" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Микроядерная архитектура" ID="ID_1819867529" CREATED="1704714080726" MODIFIED="1704714090176">
+<node TEXT="Symbian&#xa;QNX" ID="ID_686583768" CREATED="1704802172752" MODIFIED="1705921521039" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Экзоядерная архитектура" ID="ID_388796908" CREATED="1704714124758" MODIFIED="1704714133616">
+<node TEXT="ExOS&#xa;PhOS&#xa;OO OS" ID="ID_12424714" CREATED="1704802172752" MODIFIED="1705921504118" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Виртуальная машина" ID="ID_1868997245" CREATED="1704714140901" MODIFIED="1704714148377">
+<node TEXT="VMware ESX&#xa;Microsoft Hyper-V" ID="ID_519893142" CREATED="1704802172752" MODIFIED="1705921234300" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Смешанная архитектура" ID="ID_868559292" CREATED="1704714152182" MODIFIED="1704714159680">
+<node TEXT="Linux&#xa;FreeRTOS" ID="ID_269357157" CREATED="1704802172752" MODIFIED="1705921181758" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+<node TEXT="По дисциплине обслуживания" POSITION="bottom_or_right" ID="ID_326959764" CREATED="1704714377678" MODIFIED="1704714397840">
+<node TEXT="Однопользовательские" ID="ID_342233498" CREATED="1704714412102" MODIFIED="1704714420640">
+<node TEXT="MS-DOS,&#xa;Windows 3х" ID="ID_1512103774" CREATED="1704802172752" MODIFIED="1705921135461" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Многопользовательские" ID="ID_1192801277" CREATED="1704714421510" MODIFIED="1704714428960">
+<node TEXT="Пакетные" ID="ID_234724637" CREATED="1704717124526" MODIFIED="1704717134440">
+<node TEXT="OS/360&#xa;OC EC" ID="ID_525829878" CREATED="1704802172752" MODIFIED="1705921086689" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="С разделением времени" ID="ID_110266839" CREATED="1704717135902" MODIFIED="1704717143472">
+<node TEXT="UNIX&#xa;Windows Server" ID="ID_157286781" CREATED="1704802172752" MODIFIED="1705920989274" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="По поддержке процессов" POSITION="bottom_or_right" ID="ID_1272851590" CREATED="1704714578830" MODIFIED="1704714594216">
+<node TEXT="Однозадачные" ID="ID_462249344" CREATED="1704714600518" MODIFIED="1704714610384">
+<node TEXT="MS-DOS&#xa;MSX" ID="ID_990490249" CREATED="1704802172752" MODIFIED="1705920892749" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="24"/>
+</node>
+</node>
+<node TEXT="Многозадачные" ID="ID_282024640" CREATED="1704714610830" MODIFIED="1704714616216">
+<node TEXT="macOs&#xa;Windows" ID="ID_104241112" CREATED="1704802172752" MODIFIED="1705920969942" COLOR="#ff0033" BACKGROUND_COLOR="#ffffff">
+<font SIZE="36"/>
+</node>
+</node>
+</node>
+</node>
+</map>
